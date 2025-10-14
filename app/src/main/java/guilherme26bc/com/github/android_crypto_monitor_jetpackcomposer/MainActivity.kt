7af60b7eefd7b2d699f51ad4e79b61d092abbd1a
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-13 22:03:50 -0300

    inicio do projeto

---
+++ app/src/main/java/guilherme26bc/com/github/android_crypto_monitor_jetpackcomposer/MainActivity.kt
@@ -0,0 +1,47 @@
+package guilherme26bc.com.github.android_crypto_monitor_jetpackcomposer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import guilherme26bc.com.github.android_crypto_monitor_jetpackcomposer.ui.theme.Android_crypto_monitor_jetpackComposerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Android_crypto_monitor_jetpackComposerTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Android_crypto_monitor_jetpackComposerTheme {
+        Greeting("Android")
+    }
+}
